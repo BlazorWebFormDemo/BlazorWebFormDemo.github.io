f6a818d3f4d60dc4126d450c44154f13979bd4d1
Jurioli <89819793+BlazorWebFormDemo@users.noreply.github.com>
2022-05-22 01:25:32 +0800

    wwwroot

---
+++ _content/Blazor.WebForm.UI/RichTextBox.js
@@ -0,0 +1,9 @@
+﻿function RichTextBox_InputHandler(event) {
+    event.target.__hasInput = true;
+    return true;
+}
+function RichTextBox_BlurHandler(event) {
+    var hasInput = event.target.__hasInput ?? false;
+    event.target.__hasInput = false;
+    return hasInput;
+}
